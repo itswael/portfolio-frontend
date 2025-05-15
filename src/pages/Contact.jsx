dccbe05e023471211e09605e1ff2171ac0e0972e
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-05-14 22:07:34 -0400

    implemented dummy pages

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <section id="home">
+            <h1>Hi, I'm [Your Name]</h1>
+            <p>Welcome to my portfolio website!</p>
+        </section>
+    );
+};
+
+export default Home;
