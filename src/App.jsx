e486c256ea5004c0b749d2682a3991f58e21a65c
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-06-15 21:51:12 -0400

    removed redundant pages,

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
+
+import Contact from './components/Contact';
+import HeroPage from "./components/HeroPage.jsx";
 
 const App = () => {
     return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/contact" element={<Contact />} />
-            </Routes>
-        </Router>
+        <>
+            <Navbar/>
+            <HeroPage/>
+            <Contact/>
+        </>
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/global.css'; // Optional: Import global styles if needed
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
