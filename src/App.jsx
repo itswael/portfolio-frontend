5cd655ebd8c8d7b59470745bcfd4255ec65f83fe
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-06-24 22:46:37 -0400

    using dynamic titleBar component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Navbar from './components/Navbar';
 import Contact from './components/Contact';
 import HeroPage from "./components/HeroPage.jsx";
 import Footer from "./components/Footer.jsx";
+import TitleBar from "./components/titleBar.jsx";
 
 const App = () => {
     return (
         <>
+            <TitleBar/>
             <Navbar/>
             <HeroPage/>
             <Contact/>
