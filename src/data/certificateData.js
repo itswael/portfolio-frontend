3ba6d54862946d9e4acf56d8aa32345e5eb04360
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-08-02 00:36:32 -0400

    added certificate data

---
+++ src/data/certificateData.js
@@ -0,0 +1,16 @@
+export const certificates = [
+    {
+        id: 1,
+        name: "Fundamentals of Deep Learning",
+        image: "/screenshots/certificate-1.png",
+        validationUrl: "https://learn.nvidia.com/certificates?id=OAh2iXLrRMudP1smC6jkHg", // Replace with actual validation URL
+        description: ""
+    },
+    {
+        id: 2,
+        name: "Building Transformer-Based Natural Language Processing Applications",
+        image: "/screenshots/certificate-2.png",
+        validationUrl: "https://learn.nvidia.com/certificates?id=84REfrxZRyeezt1SP52Yuw", // Replace with actual validation URL
+        description: ""
+    }
+];
